74726c635128551f606c82d1d02927b602f36983
Phuc8192 <nguyenphucc10@gmail.com>
2022-12-25 01:51:14 +0700

    Loai San Pham

---
+++ PSoftware/Controllers/LoaiSanPhamsController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PSoftware.Models;
+
+namespace PSoftware.Controllers
+{
+    public class LoaiSanPhamsController : Controller
+    {
+        private PSoftwareEntities1 db = new PSoftwareEntities1();
+
+        // GET: LoaiSanPhams
+        public ActionResult Index()
+        {
+            return View(db.LoaiSanPhams.ToList());
+        }
+
+        public ActionResult IndexGame()
+        {
+            var game = db.SanPhams.Where(x => x.LoaiSanPham.TenLSP == "Game");
+            return View(game);
+        }
+
+        public ActionResult IndexSoftware()
+        {
+            var software = db.SanPhams.Where(x => x.LoaiSanPham.TenLSP == "Phần Mềm");
+            return View(software);
+        }
+
+        // GET: LoaiSanPhams/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LoaiSanPham loaiSanPham = db.LoaiSanPhams.Find(id);
+            if (loaiSanPham == null)
+            {
+                return HttpNotFound();
+            }
+            return View(loaiSanPham);
+        }
+
+        // GET: LoaiSanPhams/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LoaiSanPhams/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaLSP,TenLSP")] LoaiSanPham loaiSanPham)
+        {
+            if (ModelState.IsValid)
+            {
+                loaiSanPham.MaLSP = Guid.NewGuid();
+                db.LoaiSanPhams.Add(loaiSanPham);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(loaiSanPham);
+        }
+
+        // GET: LoaiSanPhams/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LoaiSanPham loaiSanPham = db.LoaiSanPhams.Find(id);
+            if (loaiSanPham == null)
+            {
+                return HttpNotFound();
+            }
+            return View(loaiSanPham);
+        }
+
+        // POST: LoaiSanPhams/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaLSP,TenLSP")] LoaiSanPham loaiSanPham)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(loaiSanPham).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(loaiSanPham);
+        }
+
+        // GET: LoaiSanPhams/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LoaiSanPham loaiSanPham = db.LoaiSanPhams.Find(id);
+            if (loaiSanPham == null)
+            {
+                return HttpNotFound();
+            }
+            return View(loaiSanPham);
+        }
+
+        // POST: LoaiSanPhams/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            LoaiSanPham loaiSanPham = db.LoaiSanPhams.Find(id);
+            db.LoaiSanPhams.Remove(loaiSanPham);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
